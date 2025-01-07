31d417fd385e729f88f956e5c9e86919ba2bf1ed
Cyrille <cypierre58@gmail.com>
2025-01-07 15:10:26 +0100

    sick_scan_xd: simplify config by merging dict

--- romea_lidar_bringup/launch/drivers/sick_scan_xd.launch.py
+++ romea_lidar_bringup/launch/drivers/sick_scan_xd.launch.py
@@ -50,72 +50,71 @@ def launch_setup(context, *args, **kwargs):
 
     parameters = [
         config_parameters,
-        {"nodename": "driver"},
-        # {"hostname": ip},
-        # {"port": port},
-        {"cloud_topic": "cloud"},
-        {"frame_id": frame_id},
-        {"range_filter_handling": 0},
-        {"intensity": False},
-        {"intensity_resolution_16bit": False},
-        {"use_binary_protocol": True},
-        {"timelimit": 5},
-        {"sw_pll_only_publish": True},
-        {"use_generation_timestamp": True},
-        # Use the lidar generation timestamp (true, default) or send timestamp (false)
-        # for the software pll converted message timestamp
-        {"start_services": True},
-        # start ros service for cola commands
-        {"activate_lferec": True},
-        # activate field monitoring by lferec message
-        {"activate_lidoutputstate": True},
-        # activate field monitoring by lidoutputstate messages
-        {"activate_lidinputstate": True},
-        # activate field monitoring by lidinputstate messages
-        {"min_intensity": 0.0},
-        # Set range of LaserScan messages to infinity, if intensity < min_intensity (default: 0)
-        {"add_transform_xyz_rpy": "0,0,0,0,0,0"},
-        {"message_monitoring_enabled": True},
-        # Enable message monitoring with reconnect+reinit in case of timeouts
-        {"read_timeout_millisec_default": 5000},
-        # 5 sec read timeout in operational mode (measurement mode), default: 5000 milliseconds
-        {"read_timeout_millisec_startup": 120000},
-        # 120 sec read timeout during startup (sensor may be starting up,
-        #  which can take up to 120 sec.), default: 120000 milliseconds
-        {"read_timeout_millisec_kill_node": 150000},
-        # 150 sec pointcloud timeout, ros node will be killed if no point cloud published
-        # within the last 150 sec., default: 150000 milliseconds
-        {"client_authorization_pw": "F4724744"},
-        # Default password for client authorization
+        {
+            "nodename": "driver",
+            # "hostname": ip,
+            # "port": port,
+            "cloud_topic": "cloud",
+            "frame_id": frame_id,
+            "range_filter_handling": 0,
+            "intensity": False,
+            "intensity_resolution_16bit": False,
+            "use_binary_protocol": True,
+            "timelimit": 5,
+            "sw_pll_only_publish": True,
+            "use_generation_timestamp": True,
+            # Use the lidar generation timestamp (true, default) or send timestamp (false)
+            # for the software pll converted message timestamp
+            "start_services": True,
+            # start ros service for cola commands
+            "activate_lferec": True,
+            # activate field monitoring by lferec message
+            "activate_lidoutputstate": True,
+            # activate field monitoring by lidoutputstate messages
+            "activate_lidinputstate": True,
+            # activate field monitoring by lidinputstate messages
+            "min_intensity": 0.0,
+            # Set range of LaserScan messages to infinity, if intensity < min_intensity (default: 0)
+            "add_transform_xyz_rpy": "0,0,0,0,0,0",
+            "message_monitoring_enabled": True,
+            # Enable message monitoring with reconnect+reinit in case of timeouts
+            "read_timeout_millisec_default": 5000,
+            # 5 sec read timeout in operational mode (measurement mode), default: 5000 milliseconds
+            "read_timeout_millisec_startup": 120000,
+            # 120 sec read timeout during startup (sensor may be starting up,
+            #  which can take up to 120 sec.), default: 120000 milliseconds
+            "read_timeout_millisec_kill_node": 150000,
+            # 150 sec pointcloud timeout, ros node will be killed if no point cloud published
+            # within the last 150 sec., default: 150000 milliseconds
+            "client_authorization_pw": "F4724744",
+            # Default password for client authorization
+        }
     ]
 
     if "lms1" in lidar_model:
         configuration = romea_lidar_description.sick_lms1xx_specifications(
             lidar_model, rate, resolution
         )
-        parameters.append(
-            {"min_ang": configuration["minimal_azimut_angle"] / 180.0 * math.pi}
-        )
-        parameters.append(
-            {"max_ang": configuration["maximal_azimut_angle"] / 180.0 * math.pi}
-        )
-        parameters.append({"range_min": configuration["minimal_range"]})
-        parameters.append({"range_max": configuration["maximal_range"]})
-        parameters.append({"scanner_type": "sick_lms_1xx"})
+        parameters.append({
+            "min_ang": configuration["minimal_azimut_angle"] / 180.0 * math.pi,
+            "max_ang": configuration["maximal_azimut_angle"] / 180.0 * math.pi,
+            "range_min": configuration["minimal_range"],
+            "range_max": configuration["maximal_range"],
+            "scanner_type": "sick_lms_1xx",
+            "use_binary_protocol": False,
+        })
 
     if "tim5" in lidar_model:
         configuration = romea_lidar_description.sick_tim5xx_specifications(
             lidar_model, rate, resolution
         )
-        parameters.append(
-            {"min_ang": configuration["minimal_azimut_angle"] / 180.0 * math.pi}
-        )
-        parameters.append(
-            {"max_ang": configuration["maximal_azimut_angle"] / 180.0 * math.pi}
-        )
-        parameters.append({"range_min": configuration["minimal_range"]})
-        parameters.append({"range_max": configuration["maximal_range"]})
-        parameters.append({"scanner_type": "sick_tim_5xx"})
+        parameters.append({
+            "min_ang": configuration["minimal_azimut_angle"] / 180.0 * math.pi,
+            "max_ang": configuration["maximal_azimut_angle"] / 180.0 * math.pi,
+            "range_min": configuration["minimal_range"],
+            "range_max": configuration["maximal_range"],
+            "scanner_type": "sick_tim_5xx",
+        })
 
     driver_node = Node(
         package="sick_scan_xd",
