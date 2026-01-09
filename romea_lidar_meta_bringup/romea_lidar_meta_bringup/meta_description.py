df13faa58bafd5fe3ab8b5db932074352f1aad78
Jean Laneurit <jean.laneurit@inrae.fr>
2026-01-09 14:48:50 +0100

    meta_bringup: fix generate_urdf_description
    Signed-off-by: Jean Laneurit <jean.laneurit@inrae.fr>

--- romea_lidar_meta_bringup/romea_lidar_meta_bringup/meta_description.py
+++ romea_lidar_meta_bringup/romea_lidar_meta_bringup/meta_description.py
@@ -50,7 +50,7 @@ def get_complete_configuration(meta_description):
 
 def generate_configuration_file(meta_description, extended):
     configuration = get_complete_configuration(meta_description)
-    return romea_lidar_description.generate_lidar_configuration_file(configuration, extended)
+    return romea_lidar_description.generate_configuration_file(configuration, extended)
 
 
 def generate_launch_file(meta_description):
--- romea_lidar_meta_bringup/scripts/generate_urdf_description.py
+++ romea_lidar_meta_bringup/scripts/generate_urdf_description.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
 
     mode = parameters["mode"]
     robot_namespace = parameters["robot_namespace"]
-    standalone = parameters.get("standalane", "false") == "true"
+    standalone = parameters.get("standalone", "false") == "true"
     meta_description_file_path = parameters["meta_description_file_path"]
     meta_description = LIDARMetaDescription(meta_description_file_path, robot_namespace)
     print(generate_urdf_description(mode, meta_description, standalone))
